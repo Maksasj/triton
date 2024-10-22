e8930e5fe2736bb342dfa22aa5796b69c662ebbe
Maksim Jaroslavcevas <radioboos@gmail.com>
2024-10-22 17:23:31 +0300

    workin demo

---
+++ Makefile
@@ -0,0 +1,19 @@
+
+CC=gcc
+SOURCE=triton_example.c
+OBJ=$(SOURCE:.c=.o)
+EXE=triton_example
+
+all: $(EXE) clean execute
+
+$(EXE): $(OBJ)
+	$(CC) $(OBJ) -o $@
+
+.o: .c
+	$(CC) -c $< -o $@
+
+clean:
+	rm -rf $(OBJ)
+
+execute:
+	./$(EXE)
--- triton.h
+++ triton.h
@@ -1,8 +1,56 @@
 #ifndef TRITON_H
 #define TRITON_H
 
-typedef struct {
+#include <stdio.h>
 
+typedef struct {
+    FILE* sink;
+    int scope;
 } Triton;
+
+void tri_scope(Triton* tri) {
+    for(int i = 0; i < tri->scope; ++i)
+        fprintf(tri->sink, "    ");
+}
+
+void tri_begin_object(Triton* tri) {
+    fprintf(tri->sink, "{\n");
+    ++tri->scope;
+}
+
+void tri_object_end(Triton* tri) {
+    --tri->scope;
+    tri_scope(tri);
+    fprintf(tri->sink, "}\n");
+}
+
+void tri_begin_array(Triton* tri) {
+    fprintf(tri->sink, "[\n");
+    ++tri->scope;
+}
+
+void tri_array_end(Triton* tri) {
+    --tri->scope;
+    tri_scope(tri);
+    fprintf(tri->sink, "]\n");
+}
+
+void tri_member_key(Triton* tri, const char* key) {
+    tri_scope(tri);
+
+    fprintf(tri->sink, "\"%s\": ", key);
+}
+
+void tri_bool(Triton* tri, unsigned char value) {
+    if(value) {
+        fprintf(tri->sink, "true,\n");
+    } else 
+        fprintf(tri->sink, "false,\n");
+}
+
+void tri_integer(Triton* tri, int value) {
+    tri_scope(tri);
+    fprintf(tri->sink, "%d,\n", value);
+}
 
 #endif
